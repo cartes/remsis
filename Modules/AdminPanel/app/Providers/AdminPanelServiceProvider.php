013d4ad6f65d07192fca2e0ca32195ddbc21db93
Cristian Cartes <cristiancartesa@gmail.com>
2026-02-08 19:51:11 -0300

    Route Conflict Resolution

--- Modules/AdminPanel/app/Providers/AdminPanelServiceProvider.php
+++ Modules/AdminPanel/app/Providers/AdminPanelServiceProvider.php
@@ -27,7 +27,6 @@ class AdminPanelServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
-        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'adminpanel');
     }
 
     /**
